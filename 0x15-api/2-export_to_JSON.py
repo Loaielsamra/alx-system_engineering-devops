fb73fb8936054bf69e57b0a5b0a4e8c0fb217506
Loaielsamra <loaielsamra>
2024-01-29 13:06:34 -0800

    2nd 2nd

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     todos = requests.get(url + "/todos", params={"userId": sys.argv[1]}).json()
     username = emp_data.get("username")
 
-    with open("{}.json".format(sys.argv[1]), "w", newline="") as jfile:
+    with open("{}.json".format(sys.argv[1]), "w") as jfile:
         for t in todos:
             json.dump({sys.argv[1]: [{"task": t.get("title"),
                                       "completed": t.get("completed"),
