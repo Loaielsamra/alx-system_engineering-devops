18148119ac9d8143236af9e46684b25020d17f6f
Loaielsamra <loaielsamra>
2024-01-29 13:04:56 -0800

    2nd 1st

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Returns data for a given employee id"""
+
+if __name__ == "__main__":
+
+    import json
+    import requests
+    import sys
+
+    url = "https://jsonplaceholder.typicode.com/"
+    emp_data = requests.get(url + "/users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "/todos", params={"userId": sys.argv[1]}).json()
+    username = emp_data.get("username")
+
+    with open("{}.json".format(sys.argv[1]), "w", newline="") as jfile:
+        for t in todos:
+            json.dump({sys.argv[1]: [{"task": t.get("title"),
+                                      "completed": t.get("completed"),
+                                      "username": username}]}, jfile)
