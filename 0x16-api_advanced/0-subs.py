2daee42979702cb12939fa7c1d0184fca01844f0
Loaielsamra <loaielsamra@gmail.com>
2024-02-14 20:58:38 +0200

    0th 2nd

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""Queries the reddit api and returns the number of subscribers"""
+"""Module to query the Reddit API and returns the number of subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns number of subscribers in `subreddit`"""
-
+    """Returns the number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    header = {'User-Agent': 'Mozilla/5.0'}
-
-    response = requests.get(url, headers=header, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-
-    result = response.json().get("data")
-    return result.get("subscribers")
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        return response.json().get("data").get("subscribers")
+    return 0
