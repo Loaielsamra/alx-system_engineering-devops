3a8dca30318693e1bb24dc5a66d350d342c4cf20
Loaielsamra <loaielsamra@gmail.com>
2024-02-16 20:28:11 +0200

    wave

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""Module to query the Reddit API and returns the number of subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """ tasks reddit user name and return the subscribers number """
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+    Hdrs = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=Hdrs,  allow_redirects=False)
+
     if response.status_code == 200:
-        return response.json().get("data").get("subscribers")
-    return 0
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-"""Module to query the Reddit API and returns the number of subscribers"""
-import requests
+"""this is function file"""
 
 
 def top_ten(subreddit):
-    """Returns the number of subscribers"""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        for i in range(10):
-            title = response.json()["data"]["children"][i]["data"]["title"]
-            print(title)
-    else:
-        print(None)
+    """
+    prints the titles of the first 10 hot posts listed for a given subreddit
+    """
+    import requests
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    Hdrs = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=Hdrs, allow_redirects=False)
+
+    if response.status_code != 200:
+        print("None")
+        return
+
+    data = response.json()['data']['children']
+
+    if len(data) == 0:
+        print("None")
+        return
+
+    for _ in data:
+        print(_['data']['title'])
